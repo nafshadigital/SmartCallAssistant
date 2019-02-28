3562133a8bd23636232423babdc5dce3b7e65cb6
gladguy <waheed.rahuman@gmail.com>
2019-03-01 02:21:10 +0400

    Bug fixed
    Alert Dialog
    Confirm Dialog
    
    Is your mobile number confirm
    India Timing for SMS
    Prevent Double click

--- SmartCallAssistant/app/src/main/java/com/nafshadigital/smartcallassistant/activity/EnterMobilenumber.java
+++ SmartCallAssistant/app/src/main/java/com/nafshadigital/smartcallassistant/activity/EnterMobilenumber.java
@@ -1,15 +1,16 @@
 package com.nafshadigital.smartcallassistant.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.SmsManager;
+import android.view.Gravity;
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import com.nafshadigital.smartcallassistant.R;
@@ -20,10 +21,10 @@ import com.nafshadigital.smartcallassistant.vo.UsersVO;
 import com.nafshadigital.smartcallassistant.webservice.MyRestAPI;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class EnterMobilenumber extends AppCompatActivity {
     Spinner spincountry;
@@ -42,31 +43,24 @@ public class EnterMobilenumber extends AppCompatActivity {
         txtmobileno = (EditText) findViewById(R.id.txtphnnumberverify);
         btnok = (Button) findViewById(R.id.btnokverify);
 
-        getcountry();
+        getCountry();
+        spincountry.setSelection(86); // Default to India
         spincountry.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             public void onItemSelected(AdapterView<?> arg0, View view, int position, long id) {
 
-              //CountryVO countryVO =  CountryAL.get(position);
                 CountryVO countryVO = (CountryVO) spincountry.getSelectedItem();
                 country_code = countryVO.country_code;
-             //   MyToast.show(getApplicationContext(),""+position + " - " +country_code);
-
-                    txtcountrycode.setText(countryVO.country_code);
-
+                txtcountrycode.setText(countryVO.country_code);
             }
             public void onNothingSelected(AdapterView<?> arg0) { }
         });
-
-
     }
 
-    public void getcountry() {
+    public void getCountry() {
         try {
             JSONObject jsonObject = new JSONObject();
             String res = MyRestAPI.PostCall("getCountry", jsonObject);
-           // MyToast.show(this,res);
-            System.out.println("countryRes="+res);
             CountryAL = new CountryVO().getCountryArrayList(new JSONArray(res));
             ListAdapterViewCountry adapter = new ListAdapterViewCountry(this,android.R.layout.simple_spinner_item, CountryAL);
             spincountry.setAdapter(adapter);
@@ -77,21 +71,80 @@ public class EnterMobilenumber extends AppCompatActivity {
     }
 
     public boolean isValidation(){
+
+        btnok.setEnabled(false); // Prevent the double click
+
         if (txtcountrycode.getText().toString().equals("")) {
-            MyToast.show(this, "Enter  CountryCode");
+            MyToast.show(this, "Enter Country calling code");
             return false;
         }
         if (txtmobileno.getText().toString().equals("")) {
-            MyToast.show(this, "Enter MobileNo");
-            return false;
-        }
-        if(txtmobileno.getText().length() != 9 && txtmobileno.getText().length() != 10)
-        {
-            MyToast.show(this, "Please enter proper numbers");
+            MyToast.show(this, "Enter Mobile number");
             return false;
         }
 
-        return true;
+        if(txtmobileno.getText().length() < 6 || txtmobileno.getText().length() > 10)
+        {
+            MyToast.show(this, "Enter valid Mobile number");
+            return false;
+        }
+        if(txtcountrycode.getText().toString().equals("+91"))
+        {
+            Calendar rightNow = Calendar.getInstance();
+            int currentHour = rightNow.get(Calendar.HOUR_OF_DAY);
+
+            if(currentHour < 9 || currentHour > 21) {
+                alertWindow("India SMS Timing","SMS can only be delivered between 9 A.M and 9 P.M");
+                return false;
+            }
+        }
+        String mobileNumber = txtcountrycode.getText().toString() + "-" + txtmobileno.getText().toString();
+        String confirmMessage = "Is this your correct phone number? \n" + mobileNumber;
+        String windowTitle = "Confirm Your Number";
+
+        return confirmWindow(windowTitle,confirmMessage);
+    }
+
+    public boolean confirmWindow(String title, String message)
+    {
+        final boolean[] userAnswer = {false};
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(message);
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        userAnswer[0] = true;
+                    }
+                });
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        userAnswer[0] = false;
+                    }
+                });
+        alertDialog.show();
+        return  userAnswer[0];
+    }
+    public void alertWindow(String title,String message)
+    {
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(message);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        alertDialog.show();
+        //Center the OK Button
+        final Button positiveButton = alertDialog.getButton(AlertDialog.BUTTON_NEUTRAL);
+        LinearLayout.LayoutParams positiveButtonLL = (LinearLayout.LayoutParams) positiveButton.getLayoutParams();
+        positiveButtonLL.gravity = Gravity.CENTER;
+        positiveButton.setLayoutParams(positiveButtonLL);
+
     }
     public void okverify(View view)
     {
